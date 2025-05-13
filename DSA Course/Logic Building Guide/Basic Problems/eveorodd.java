41fdbe394568810ff4d9319c30797700b987cce2
Prachi Anand <prachianand321@gmail.com>
2025-05-13 13:31:25 +0530

    Check whether a given number is even or odd using bitwise shift operators in java

--- DSA Course/Logic Building Guide/Basic Problems/eveorodd.java
+++ DSA Course/Logic Building Guide/Basic Problems/eveorodd.java
@@ -6,8 +6,9 @@ public class eveorodd{
         
         int n = sc.nextInt();
 
-        System.out.println("By Finding the Remainder" + fn(n));
-        System.out.println("By using Bitwise AND operator" + bitwiseand(n));
+        System.out.println("By Finding the Remainder: " + fn(n));
+        System.out.println("By using Bitwise AND operator: " + bitwiseand(n));
+        System.out.println("By using Bitwise shift operators: " + bitwiseshift(n));
     }
 
     public static boolean fn(int n){
@@ -19,8 +20,17 @@ public class eveorodd{
         }
     }
 
-    public static bitwiseand(int n){
+    public static boolean bitwiseand(int n){
         if((n&1) == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static boolean bitwiseshift(int n){
+        if( ((n >> 1) << 1) == n){
             return true;
         }
         else{
