9729ee11382f4a676039b903e4156214dac85287
Prachi Anand <prachianand321@gmail.com>
2025-05-13 13:26:41 +0530

    Check whether a given number is even or odd using bitwise AND in java

--- DSA Course/Logic Building Guide/Basic Problems/eveorodd.java
+++ DSA Course/Logic Building Guide/Basic Problems/eveorodd.java
@@ -6,11 +6,21 @@ public class eveorodd{
         
         int n = sc.nextInt();
 
-        System.out.println(fn(n));
+        System.out.println("By Finding the Remainder" + fn(n));
+        System.out.println("By using Bitwise AND operator" + bitwiseand(n));
     }
 
     public static boolean fn(int n){
         if(n % 2 == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static bitwiseand(int n){
+        if((n&1) == 0){
             return true;
         }
         else{
